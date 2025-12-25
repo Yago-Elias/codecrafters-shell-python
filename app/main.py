876d120615ccfd83b54d8ba98509774aa173174a
Yago Elias <yagoelias024@gmail.com>
2025-12-25 00:08:43 -0300

    adicionar o comando echo

--- app/main.py
+++ app/main.py
@@ -4,10 +4,14 @@ import sys
 def main():
     while (True):
         sys.stdout.write("$ ")
-        command = input()
-        if command == 'exit':
+        command = input().split(' ')
+
+        if command[0] == 'exit':
             return
-        print(f'{command}: command not found')
+        elif command[0] == 'echo':
+            print(' '.join(command[1:]))
+        else:
+            print(f'{command[0]}: command not found')
 
 
 if __name__ == "__main__":
