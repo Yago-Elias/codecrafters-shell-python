44192e1b26eba21c538f0dd122bc9d441fe9fb79
Yago Elias <yagoelias024@gmail.com>
2025-12-25 00:27:53 -0300

    adicionar o comando type

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ import sys
 
 
 def main():
+    commands = ['exit', 'echo', 'type']
+
     while (True):
         sys.stdout.write("$ ")
         command = input().split(' ')
@@ -10,6 +12,9 @@ def main():
             return
         elif command[0] == 'echo':
             print(' '.join(command[1:]))
+        elif command[0] == 'type':
+            print(f'{command[1]}', end='')
+            print(' is a shell builtin' if command[1] in commands else ': not found')
         else:
             print(f'{command[0]}: command not found')
 
